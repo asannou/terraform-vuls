a239c42236a7a261d722d0f4deba2f51f05b6667
asannou <hataori@gmail.com>
2020-04-06 19:41:05 +0900

    update versions of each distribution in OVAL

--- scanner/vuls.sh
+++ scanner/vuls.sh
@@ -254,9 +254,9 @@ run_cve fetchnvd -last2y
 run_cve fetchjvn -last2y
 
 fetch_oval fetch-debian 7 8 9 10
-fetch_oval fetch-redhat 5 6 7
-fetch_oval fetch-ubuntu 12 14 16 18 19
-fetch_oval fetch-alpine 3.3 3.4 3.5 3.6 3.7 3.8 3.9 3.10
+fetch_oval fetch-redhat 5 6 7 8
+fetch_oval fetch-ubuntu 14 16 18 19 20
+fetch_oval fetch-alpine 3.3 3.4 3.5 3.6 3.7 3.8 3.9 3.10 3.11
 fetch_oval fetch-amazon
 
 build_images
