96471b6921c53eefb3000e60119bcb60d3d62b4d
asannou <hataori@gmail.com>
2020-04-20 20:14:19 +0900

    update sudoers

--- target/create_vuls_user.sh
+++ target/create_vuls_user.sh
@@ -4,7 +4,7 @@ USER=vuls
 DOTSSH=/home/$USER/.ssh
 COMMAND=$DOTSSH/vuls-ssh-command.sh
 
-useradd -m $USER
+useradd -m $USER 2> /dev/null
 mkdir -m 700 $DOTSSH
 aws s3 cp {{sshcommand}} $COMMAND > /dev/null
 echo "command=\"$COMMAND\" {{publickey}}" > $DOTSSH/authorized_keys
@@ -15,15 +15,27 @@ cat /etc/ssh/ssh_host_ecdsa_key.pub
 
 exec > /dev/null
 
-if [ -e /etc/redhat-release ]
-then
-  printf '%s\n' $USER' ALL=(ALL) NOPASSWD:/usr/bin/yum --color=never repolist, /usr/bin/yum --color=never --security updateinfo list updates, /usr/bin/yum --color=never --security updateinfo updates, /usr/bin/repoquery, /usr/bin/yum --color=never changelog all *' 'Defaults:'$USER' env_keep="http_proxy https_proxy HTTP_PROXY HTTPS_PROXY"' > /etc/sudoers.d/vuls
-fi
+# https://vuls.io/docs/en/usage-configtest.html#etc-sudoers
 
-if [ -e /etc/debian_version ]
-then
-  printf '%s\n' $USER' ALL=(ALL) NOPASSWD: /usr/bin/apt-get update' 'Defaults:'$USER' env_keep="http_proxy https_proxy HTTP_PROXY HTTPS_PROXY"' > /etc/sudoers.d/vuls
-fi
+test -e /etc/debian_version && cat > /etc/sudoers.d/vuls <<__EOD__
+$USER ALL=(ALL) NOPASSWD: /usr/bin/apt-get update, /usr/bin/stat *, /usr/sbin/checkrestart, /bin/ls -l /proc/*/exe, /bin/cat /proc/*/maps, /usr/bin/lsof -i -P -n
+Defaults:$USER env_keep="http_proxy https_proxy HTTP_PROXY HTTPS_PROXY"
+__EOD__
+
+test -e /etc/centos-release || grep 'Amazon Linux' /etc/system-release > /dev/null 2>&1 && cat > /etc/sudoers.d/vuls <<__EOD__
+$USER ALL=(ALL) NOPASSWD: /usr/bin/stat, /usr/bin/needs-restarting, /usr/bin/which, /bin/ls -l /proc/*/exe, /bin/cat /proc/*/maps, /usr/sbin/lsof -i -P -n
+Defaults:$USER env_keep="http_proxy https_proxy HTTP_PROXY HTTPS_PROXY"
+__EOD__
+
+test -e /etc/redhat-release && cat > /etc/sudoers.d/vuls <<__EOD__
+$USER ALL=(ALL) NOPASSWD: /usr/bin/stat, /usr/bin/needs-restarting, /usr/bin/which, /usr/bin/repoquery, /usr/bin/yum makecache, /bin/ls -l /proc/*/exe, /bin/cat /proc/*/maps, /usr/bin/lsof -i -P -n, /usr/sbin/lsof -i -P -n
+Defaults:$USER env_keep="http_proxy https_proxy HTTP_PROXY HTTPS_PROXY"
+__EOD__
+
+test -e /etc/oracle-release && cat > /etc/sudoers.d/vuls <<__EOD__
+$USER ALL=(ALL) NOPASSWD: /usr/bin/stat, /usr/bin/needs-restarting, /usr/bin/which, /usr/bin/repoquery, /usr/bin/yum makecache
+Defaults:$USER env_keep="http_proxy https_proxy HTTP_PROXY HTTPS_PROXY"
+__EOD__
 
 if type docker
 then
