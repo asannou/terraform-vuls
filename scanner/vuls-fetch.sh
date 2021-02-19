e3884b392aa818f177f975af9a898bbb0a4c5fc5
asannou <hataori@gmail.com>
2021-02-19 19:12:47 +0900

    support gost

--- scanner/vuls-fetch.sh
+++ scanner/vuls-fetch.sh
@@ -4,6 +4,7 @@ set -eu
 
 DOCKER_CVE_IMAGE=vuls/go-cve-dictionary
 DOCKER_OVAL_IMAGE=vuls/goval-dictionary
+DOCKER_GOST_IMAGE=vuls/gost
 
 run_cve() {
   docker pull $DOCKER_CVE_IMAGE
@@ -16,7 +17,20 @@ run_cve() {
 
 fetch_oval() {
   docker pull $DOCKER_OVAL_IMAGE
-  docker run --rm -i -v $PWD:/vuls -v $PWD/goval-dictionary-log:/var/log/vuls $DOCKER_OVAL_IMAGE "$@"
+  docker run --rm -i \
+    -v $PWD:/vuls \
+    -v $PWD/goval-dictionary-log:/var/log/vuls \
+    $DOCKER_OVAL_IMAGE \
+    "$@"
+}
+
+fetch_gost() {
+  docker pull $DOCKER_GOST_IMAGE
+  docker run --rm -i \
+    -v $PWD:/vuls \
+    -v $PWD/gost-log:/var/log/gost \
+    $DOCKER_GOST_IMAGE \
+    fetch "$@"
 }
 
 run_cve fetchnvd -last2y
@@ -25,6 +39,8 @@ run_cve fetchjvn -last2y
 fetch_oval fetch-debian 7 8 9 10
 fetch_oval fetch-redhat 5 6 7 8
 fetch_oval fetch-ubuntu 14 16 18 19 20
-fetch_oval fetch-alpine 3.3 3.4 3.5 3.6 3.7 3.8 3.9 3.10 3.11
+fetch_oval fetch-alpine 3.3 3.4 3.5 3.6 3.7 3.8 3.9 3.10 3.11 3.12 3.13
 fetch_oval fetch-amazon
+
+fetch_gost debian
 
