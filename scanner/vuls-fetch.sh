d3aaac8a7fab145c83417f4d917a1ba03235470e
asannou <hataori@gmail.com>
2021-11-08 19:47:41 +0900

    fix docker image versions

--- scanner/vuls-fetch.sh
+++ scanner/vuls-fetch.sh
@@ -2,9 +2,9 @@
 
 set -eu
 
-DOCKER_CVE_IMAGE=vuls/go-cve-dictionary
-DOCKER_OVAL_IMAGE=vuls/goval-dictionary
-DOCKER_GOST_IMAGE=vuls/gost
+DOCKER_CVE_IMAGE=vuls/go-cve-dictionary:0.6.2
+DOCKER_OVAL_IMAGE=vuls/goval-dictionary:0.3.5
+DOCKER_GOST_IMAGE=asannou/gost:v0.1.10
 
 run_cve() {
   docker pull $DOCKER_CVE_IMAGE
@@ -36,10 +36,10 @@ fetch_gost() {
 run_cve fetchnvd -last2y
 run_cve fetchjvn -last2y
 
-fetch_oval fetch-debian 7 8 9 10
+fetch_oval fetch-debian 7 8 9 10 11
 fetch_oval fetch-redhat 5 6 7 8
-fetch_oval fetch-ubuntu 14 16 18 19 20
-fetch_oval fetch-alpine 3.3 3.4 3.5 3.6 3.7 3.8 3.9 3.10 3.11 3.12 3.13
+fetch_oval fetch-ubuntu 14 16 18 19 20 21
+fetch_oval fetch-alpine 3.3 3.4 3.5 3.6 3.7 3.8 3.9 3.10 3.11 3.12 3.13 3.14
 fetch_oval fetch-amazon
 
 fetch_gost debian
